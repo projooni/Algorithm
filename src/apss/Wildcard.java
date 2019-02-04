6a35c9c4019aa2fcfc708f54112e0413f5d90b85
projo <projo@DESKTOP-RAH32Q2>
2019-02-04 17:52:19 +0900

    WILDCARD 풀이보고 품

--- src/apss/Wildcard.java
+++ src/apss/Wildcard.java
@@ -6,17 +6,21 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Wildcard {
 	
 	public static String W;
 	public static int N;
 	public static String fileName[];
+	public static List<String> result;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
 		
-		System.setIn(new FileInputStream(""));
+		System.setIn(new FileInputStream("C://eclipse-workspace/sample_input.txt"));
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -27,18 +31,79 @@ public class Wildcard {
 			W = br.readLine().trim();
 			N = Integer.parseInt(br.readLine().trim());
 			
+			result = new ArrayList<String>();
+			
+			fileName = new String[N];
 			for(int i=0; i<N; i++) {
 				fileName[i] = br.readLine().trim();
+				
+				bw.flush();
+				
+				if(search(0, 0, fileName[i]) == 1) {
+					result.add(fileName[i]);
+					
+				}
+				
 			}
 			
+			Collections.sort(result);
 			
-			
-			
+			for(int i=0; i<result.size(); i++) {
+				bw.write(result.get(i) + "\n");
+			}
 			
 		}
+		bw.close();
 		
 		
 
+	}
+	
+	// 
+	public static int search(int pIdx, int fIdx, String fileName) {
+		
+		if(fileName.length() < 1) {
+			return 1;
+		}
+		
+		int ret = 1;
+		
+		// 패턴과 비교
+		for(int pi=pIdx; pi<W.length(); pi++) {
+			if(fIdx >= fileName.length()) {
+				break;
+			}
+			// 현재 패턴 문자
+			char p = W.charAt(pi);
+			// 패턴과 다른경우
+			if(p != fileName.charAt(fIdx)) {
+				if(p == '?') {
+					fIdx++;
+					continue;
+				}else if(p == '*') {
+					for(int i=pi+1; i<fileName.length(); i++) {
+						ret = search(pi+1, i, fileName);
+						if(ret == 1) {
+							return 1;
+						}
+					}
+				}else{
+					ret = 0;
+					break;
+				}
+			}else {
+				// 패턴과 일치하는데, 패턴 인덱스만 끝에 도달한경우
+				if(pi == (W.length()-1) && fIdx < (fileName.length()-1)) {
+					ret = 0;
+					break;
+				}
+				fIdx++;
+			}
+		}
+		
+		return ret;
+		
+		
 	}
 
 }
---
+++ src/apss/Wildcard_Solution.java
@@ -0,0 +1,133 @@
+package apss;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Wildcard_Solution {
+	
+	public static String W, S;
+	public static int N;
+	public static int cache[][];
+	public static String fileName[];
+	public static List<String> result;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		
+		System.setIn(new FileInputStream("C://eclipse-workspace/sample_input.txt"));
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int T = Integer.parseInt(br.readLine().trim());
+		for(int testCase=1; testCase<=T; testCase++) {
+			
+			W = br.readLine().trim();
+			N = Integer.parseInt(br.readLine().trim());
+			
+			cache = new int[101][101];
+			for(int i=0; i<101; i++) {
+				for(int j=0; j<101; j++) {
+					cache[i][j] = -1;
+				}
+			}
+			
+			result = new ArrayList<String>();
+			
+			fileName = new String[N];
+			for(int i=0; i<N; i++) {
+				fileName[i] = br.readLine().trim();
+				S = fileName[i];
+				
+				bw.flush();
+				
+				if(dynamicMatch(0,0) == 1) {
+					result.add(fileName[i]);
+					
+				}
+				
+			}
+			
+			Collections.sort(result);
+			
+			for(int i=0; i<result.size(); i++) {
+				bw.write(result.get(i) + "\n");
+			}
+			
+		}
+		bw.close();
+		
+		
+
+	}
+	
+	// 와일드카드 패턴 w가 문자열 s에 대응되는지 여부를 반환한다.
+	public static boolean match(String w, String s) {
+		// w[pos]와 s[pos]를 맞춰나간다.
+		int pos = 0;
+		while(pos < s.length() && pos<w.length() && (w.charAt(pos) == '?' || w.charAt(pos) == s.charAt(pos))) {
+			pos++;
+		}
+		
+		// 더이상 대응할 수 없으면 왜 while문이 끝났는지 확인한다.
+		// 2. 패턴 끝에 도달해서 끝난 경우: 문자열도 끝났어야 대응됨
+		if(pos == w.length()) {
+			return pos == s.length();
+		}
+		
+		// 4. *를 만나서 끝난 경우: *에 몇 글자를 대응해야 할지 재귀 호출하면서 확인한다.
+		if(w.charAt(pos) == '*') {
+			for(int skip=0; pos+skip<=s.length(); skip++) {
+				if(match(w.substring(pos+1), s.substring(pos+skip))) {
+					return true;
+				}
+			}
+		}
+		
+		// 이 외의 경우에는 모두 대응되지 않는다.
+		return false;
+	}
+	
+	public static int dynamicMatch(int w, int s) {
+		// -1은 아직 답이 계산되지 않았음을 의미한다.
+		// 1은 해당 입력들이 서로 대응됨을 의미한다.
+		// 0은 해당 입력들이 서로 대응되지 않음을 의미한다.
+		int ret = cache[w][s];
+		if(ret != -1) {
+			return cache[w][s];
+		}
+		
+		// W[w]와 S[s]를 맞춰나간다.
+		while(w<W.length() && s<S.length() && (W.charAt(w) == '?' || W.charAt(w) == S.charAt(s))) {
+			w++;
+			s++;
+		}
+		
+		// 더이상 대응할 수 없으면 왜 while문이 끝났는지 확인한다
+		// 2. 패턴 끝에 도달해서 끝난 경우: 문자열도 끝났어야 참
+		if(w == W.length()) {
+			return ret = (s == S.length() ? 1 : 0);
+		}
+		
+		// 4. *를 만나서 끝난 경우: *에 몇 글자를 대응해야 할지 재귀 호출하면서 확인한다.
+		if(W.charAt(w) == '*') {
+			for(int skip=0; skip+s <= S.length(); skip++) {
+				if(dynamicMatch(w+1, s+skip) == 1) {
+					return ret = 1;
+				}
+			}
+		}
+		
+		// 3. 이 외의 경우에는 모두 대응되지 않는다.
+		return ret = 0;
+		
+	}
+
+}
