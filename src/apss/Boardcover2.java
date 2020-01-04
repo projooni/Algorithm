3432c67e8add4e7df640b940ccc7253215f752f6
projooni <projooni@172.30.1.41>
2020-01-04 23:25:46 +0900

    게임판덮기2(BOARDCOVER2) - 문제는 풀었으나 시간초

--- src/apss/Boardcover2.java
+++ src/apss/Boardcover2.java
@@ -1,5 +1,11 @@
 package apss;
 
+/*
+ * 게임판덮기2 (BOARDCOVER2)
+ * 결과: 시간초과 (답은 맞는듯?) block 중복제거구현이 미흡하지 않았나 싶
+ * 풀이과정 : 책보고 풀었음.
+ * */
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileInputStream;
@@ -15,14 +21,15 @@ public class Boardcover2 {
 	public static int W,H,R,C;
 	public static int block[][];
 	public static int board[][];
+	public static boolean dup[];
 	public static List<int[]> rotations[];
 	public static int best;
 	public static int blockSize;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stubs
 
-		System.setIn(new FileInputStream("/User/projooni/sample_input.txt"));
+		System.setIn(new FileInputStream("/Users/projooni/sample_input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
@@ -36,7 +43,9 @@ public class Boardcover2 {
 			C = Integer.parseInt(st.nextToken());
 			
 			board = new int[H][W];
+			dup = new boolean[4];
 			block = new int[R][C];
+			rotations = new ArrayList[4];
 			rotations[0] = new ArrayList<int[]>();
 			rotations[1] = new ArrayList<int[]>();
 			rotations[2] = new ArrayList<int[]>();
@@ -69,7 +78,7 @@ public class Boardcover2 {
 			}
 			
 			generateRotations(block);
-//			search(0);
+			search(0);
 			
 			bw.flush();
 			bw.write(best + "\n");
@@ -78,7 +87,7 @@ public class Boardcover2 {
 		
 		bw.close();
 
-	}
+	} 
 	
 	// 이것만으로도 머리터짐
 	// arr(block)을 시계방향으로 90도 회전한다. 
@@ -98,7 +107,7 @@ public class Boardcover2 {
 		for(int rot=0; rot<4; rot++) {
 			int originY = -1;
 			int originX = -1;
-			for(int i=0; i<block.length; i++) {
+			for(int i=0; i<block.length; ++i) {
 				for(int j=0; j<block[i].length; j++) {
 					if(block[i][j] == 1) {
 						if(originY == -1) {
@@ -109,15 +118,29 @@ public class Boardcover2 {
 						// 각 칸의 위치를 원점으로부터의 상대좌표로 표현한다.
 						rotations[rot].add(new int[] {i-originY, j-originX});
 					}
-					// block을 시계 방향으로 90도 회전한다.
-					block = rotate(block);
+					
 				}
 			}
-			// 네 가지 회전 형태 중 중복이 있을 경우 이를 제거한다. (일단 생략)
 			
-			// 블록이 몇 칸 짜리인지 저장해둔다.
-			blockSize = rotations[0].size();
+			// block을 시계 방향으로 90도 회전한다.
+			block = rotate(block);
+			
 		}
+		
+		// 네 가지 회전 형태 중 중복이 있을 경우 이를 제거한다. (일단 생략)
+		for(int i=0; i<4; i++) {
+			for(int j=0; j<4; j++) {
+				if(i == j) {
+					continue;
+				}
+				if(!dup[i] && rotations[i].containsAll(rotations[j])) {
+					dup[j] = true;
+				}
+			}
+		}
+		
+		// 블록이 몇 칸 짜리인지 저장해둔다.
+		blockSize = rotations[0].size();
 		
 	}
 	
@@ -125,57 +148,67 @@ public class Boardcover2 {
 	// board의 (y,x)를 type번 방법으로 덮거나, 덮었언 블록을 없앤다.
 	// delta = 1 이면 덮고, -1이면 덮었던 블록을 없앤다.
 	// 만약 블록이 제대로 덮이지 않은 경우 (게임판 밖으로 나가거나, 겹치거나, 검은 칸을 덮을 때) false 반환한다.
-//	public static boolean set(int y, int x, int type, int delta) {
-//		boolean ok = true;
-//		for(int i=0; i<3; i++) {
-//			int ny = y + coverType[type][i][0];
-//			int nx = x + coverType[type][i][1];
-//			if(ny < 0 || ny >= H || nx < 0 || nx >= W) {
-//				ok = false;
-//			}else if((board[ny][nx] += delta) > 1) {
-//				ok = false;
-//			}
-//		}
-//		
-//		return ok;
-//	}
+	public static boolean set(int y, int x, int type, int delta) {
+		boolean ok = true;
+		List<int[]> rotation = rotations[type];
+		
+		for(int i=0; i<rotation.size(); i++) {
+			int[] n = rotation.get(i);
+			int ny = y+n[0];
+			int nx = x+n[1];
+			if(ny < 0 || ny >= H || nx < 0 || nx >= W) {
+				ok = false;
+			}else if((board[ny][nx] += delta) > 1) {
+				ok = false;
+			}
+		}
+		
+		return ok;
+	}
 	
 	// placed: 지금까지 놓은 블록의 수 
-//	public static void search(int placed) {
-//		// 아직 채우지 못한 칸 중 가장 윗줄 왼쪽에 있는 칸을 찾는다.
-//		int y = -1, x = -1;
-//		for(int i=0; i<H; i++) {
-//			for(int j=0; j<W; j++) {
-//				if(board[i][j] == 0) {
-//					y = i;
-//					x = j;
-//					break;
-//				}
-//			}
-//			if(y != -1) {
-//				break;
-//			}
-//		}
-//		
-//		// 기저 사례 : 모든 칸을 채웠으면 1을 반환한다.
-//		if(y == -1) {
-//			best = Math.max(best, placed);
-//			return;
-//		}
-//		
-//		int ret = 0;
-//		for(int type = 0; type < 4; type++) {
-//			// 만약 board[y][x]를 type 형태로 덮을 수 있으면 재귀 호출한다.
-//			if(set(y,x,type,1)) {
-//				ret += cover();
-//			}
-//			// 덮었던 블록을 치운다.
-//			set(y,x,type,-1);
-//		}
-//		
-//		return ret;
-//		
-//	}
+	public static void search(int placed) {
+		// 아직 채우지 못한 칸 중 가장 윗줄 왼쪽에 있는 칸을 찾는다.
+		int y = -1, x = -1;
+		for(int i=0; i<H; i++) {
+			for(int j=0; j<W; j++) {
+				if(board[i][j] == 0) {
+					y = i;
+					x = j;
+					break;
+				}
+			}
+			if(y != -1) {
+				break;
+			}
+		}
+		
+		// 기저 사례 : 게임판의 모든 칸을 처리한 경우 
+		if(y == -1) {
+			best = Math.max(best, placed);
+			return;
+		}
+		
+		for(int type = 0; type < 4; type++) {
+			if(dup[type]) {
+				continue;
+			}
+			// 만약 board[y][x]를 type 형태로 덮을 수 있으면 재귀 호출한다.
+			if(set(y,x,type,1)) {
+				search(placed+1);
+			}
+			// 덮었던 블록을 치운다.
+			set(y,x,type,-1);
+		}
+		
+		// 이 칸을 덮지 않고 '막아' 둔다.
+		board[y][x] = 1;
+		search(placed);
+		board[y][x] = 0;
+		
+		return;
+		
+	}
 	
 
 }
