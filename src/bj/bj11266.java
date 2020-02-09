b7ed969be6cb6937d5a4a1885f823c9876ddcb54
projooni <projooni@192.168.0.100>
2020-02-09 20:01:38 +0900

    단절점 완료..

--- src/bj/bj11266.java
+++ src/bj/bj11266.java
@@ -10,6 +10,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
+/*더 완벽한 이해가 필요함. 블로그 리뷰할때 완벽 내재화 필요.*/
+
 public class bj11266 {
 	
 	public static int V, E;
@@ -48,9 +50,10 @@ public class bj11266 {
 				adjList[b].add(a);
 			}
 			
-			cnt = 0;
+			// dfsAll -> component가 하나일 것이므로 사실 의미는 없다.
 			for(int i=1; i<=V; i++) {
 				if(discover[i] == -1) {
+					cnt = 0;
 					dfs(i, -1, true);
 				}
 			}
@@ -75,24 +78,27 @@ public class bj11266 {
 	}
 	
 	public static int dfs(int vertex, int parent, boolean isRoot) {
-		
-//		if(discover[vertex] != -1) {
-//			return discover[vertex];
-//		}
 
 		// 발견 카운트를 기록한다.
 		int ret = discover[vertex] = cnt++;
-		
-		int childCount = adjList[vertex].size();
+		// 자식 수를 저장하는데, 조심해야할건 원본 그래프가 아닌 스패닝 트리의 자식 수이다.
+		int childCount = 0;
+		// vertex가 단절점인지 여
 		boolean isArticulation = false;
-		for(int i=0; i<childCount; i++) {
+		
+		// 자식노드 순회 시작.
+		for(int i=0; i<adjList[vertex].size(); i++) {
 			int next = adjList[vertex].get(i);
 			
+			// 다음 노드가 부모 노드라면 무시한다.
 			if(next == parent) {
 				continue;
 			}
 			
+			// 발견된적 없는 노드의 경우 
 			if(discover[next] == -1) {
+				// 자식 카운트를 늘린다.
+				childCount++;
 				// next를 방문하고, 최소 발견카운트를 리턴한다.
 				int minDiscover = dfs(next, vertex, false);
 				
@@ -108,6 +114,7 @@ public class bj11266 {
 			
 		}
 		
+		// 루트 노드 이면서 스패닝 트리의 자식 카운트가 2 이상인 경우 무조건 단절점이다.
 		if((isRoot && childCount >= 2) || isArticulation) {
 			result[vertex] = true;
 		}
