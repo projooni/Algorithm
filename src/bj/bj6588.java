02efef311819cbce302d58f43cae28bb1e001d8e
projo <projo@DESKTOP-4P240SJ>
2020-05-06 23:16:18 +0900

    bj6588
    - 에라토스테네스의 체 구현
    - 완료

--- src/bj/bj6588.java
+++ src/bj/bj6588.java
@@ -22,13 +22,15 @@ public class bj6588 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
+		// 에라토스테네스의 체는 최대 백만까지 한번만 계산하면 된다
+		eratosthenes(1000000);
+		
 		while(true){
 			N = Integer.parseInt(br.readLine());
 			if(N == 0) {
 				break;
 			}
 			
-			eratosthenes();
 			String result = solve();
 			
 			bw.flush();
@@ -39,70 +41,47 @@ public class bj6588 {
 
 	}
 	
-	public static void eratosthenes() {
+	public static void eratosthenes(int n) {
 		
-		arr = new boolean[N+1];
+		// 소수는 true, 그외는 false
+		arr = new boolean[n+1];
+		
+		// 0과 1은 소수가 아니다
 		arr[0] = false;
 		arr[1] = false;
+		
+		// 2부터 끝자리까지 일단 모두 소수라고 가정한다
 		for(int i=2; i<arr.length; i++) {
 			arr[i] = true;
 		}
 		
+		// 2부터 마지막 숫자까지 순회하면서 자기 자신을 제외한 배수를 제거한다 (에라토스테네스의 체)
 		for(int i=2; i<arr.length; i++) {
 			if(!arr[i]) {
 				continue;
 			}
 			for(int j=i+i; j<arr.length; j+=i) {
-				if(!arr[j]) {
-					continue;
-				}
 				arr[j] = false;
 			}
 		}
 		
-		for(int i=3; i<arr.length; i++) {
-			if(arr[i] && i%2 == 1) {
-				arrList.add(i);
-			}
-		}
-		
-		
 	}
 	
 	public static String solve() {
+		
 		String ret = "Goldbach's conjecture is wrong.\n";
 		
-		for(int i=0; i<arrList.size(); i++) {
-			int value = arrList.get(i);
-			if(arr[N-value]) {
-				ret = N + " = " + value + " + " + (N-value) + "\n";
+		/* 
+		 * (1) 2는 유일한 짝수 소수이므로 제외하고 3부터 순회
+		 * (2) i + (N-i) = N 이므로, 이 조합만 답이되는지 체크한다. 
+		 * (3) 2번의 이유로 N/2까지만 순회하면 된다.
+		 */
+		for(int i=3; i<=N/2; i++) {
+			if(arr[i] && arr[N-i]) {
+				ret = N + " = " + i + " + " + (N-i) + "\n";
 				break;
 			}
 		}
-		
-		/*
-		for(int i=2; i<=N/2; i++) {
-			if((arr[i] != -1) && (arr[N-i] != -1)) {
-				ret[0] = N-i;
-				ret[1] = i;
-				break;
-			}
-		}
-		*/
-		
-//		for(int i=arrList.size()-1; i>=0; i--) {
-//			int max_value = arrList.get(i);
-//			for(int j=0; j<arrList.size(); j++) {
-//				if(i == j) {
-//					continue;
-//				}
-//				int min_value = arrList.get(j);
-//				if(min_value + max_value == N) {
-//					ret[0] = max_value;
-//					ret[1] = min_value;
-//				}
-//			}
-//		}
 		
 		return ret;
 		
